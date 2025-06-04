6e406805a41de88604f9f1e584ee1112ad64afa1
Christoph Häni <christoph.haeni@bfh.ch>
2025-06-04 21:24:33 +0200

    Switch to selfmade GET calls

--- scripts/0-test-api.r
+++ scripts/0-test-api.r
@@ -1,22 +1,41 @@
 
-library(rstac)
+library(httr)
 
-bu <- 'https://data.geo.admin.ch/api/stac/v1/'
-# bu <- 'https://data.geo.admin.ch/api/stac/v1/collections/ch.meteoschweiz.ogd-smn'
+# REST API docu
+# https://data.geo.admin.ch/api/stac/static/spec/v1/api.html#tag/Data
 
-src <- stac(bu)
+# fetch all available MeteoSwiss Open Data collections
+meteoswiss_collections <- function() {
+    # base url to REST API
+    bu <- 'https://data.geo.admin.ch/api/stac/v1/'
+    out <- content(GET(paste0(bu, 'collections?provider=meteoswiss')))
+    if (is.null(out$code)) {
+        structure(out, class = c('ms_collections', class(out)))
+    } else {
+        out
+    }
+}
 
-get_request(src)
+# print method for collections
+print.ms_collections <- function(x, ...) {
+    # get ids & titles
+    ids <- sapply(x$collections, '[[', 'id')
+    titles <- sapply(x$collections, '[[', 'title')
+    mt <- max(nchar(titles)) + grepl('[^a-zA-Z0-9:)( -]', titles) * 2
+    cat('~~~~~~\n')
+    cat('Open Data - MeteoSwiss\n')
+    cat('----------------------\n')
+    cat('available collections:\n')
+    for (i in seq_along(ids)) {
+        cat(sprintf(
+                paste0('%2i: %-', mt[i], 's -> %s\n')
+                , i, titles[i], ids[i]))
+    }
+    cat('~~~~~~\n')
+    invisible()
+}
 
-str(src)
+# test
+cl <- meteoswiss_collections()
+cl
 
-cl <- collections(src)
-
-get_request(cl)
-
-mc <- stac_search(
-    q = src,
-    collections = 'ch.meteoschweiz.ogd-smn'
-)
-
-get_request(mc)
