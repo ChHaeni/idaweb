c2f240138eb7f1d88772983cd59d396d80023320
Christoph Häni <christoph.haeni@bfh.ch>
2025-06-04 22:08:25 +0200

    Play around

--- scripts/0-test-api.r
+++ scripts/0-test-api.r
@@ -43,3 +43,48 @@ cl
 cl[20]
 
 
+# fetch assets of a single collection
+get_assets <- function(id = NULL) {
+    # check id input
+    if (!(length(id) == 1 && is.character(id))) {
+        stop('argument "id" is not a valid single collection id!')
+    }
+    # base url to REST API
+    bu <- 'https://data.geo.admin.ch/api/stac/v1/'
+    out <- content(GET(paste0(bu, 'collections/', id, '/assets')))
+    if (is.null(out$code)) {
+        sapply(out$assets, '[[', 'id')
+    } else {
+        out
+    }
+}
+
+yy <- get_assets('ch.meteoschweiz.ogd-smn')
+lapply(cl, get_assets)
+
+str(yy$assets)
+
+# fetch list of items for a single collection 
+get_items <- function(id = NULL) {
+    # check id input
+    if (!(length(id) == 1 && is.character(id))) {
+        stop('argument "id" is not a valid single collection id!')
+    }
+    # base url to REST API
+    bu <- 'https://data.geo.admin.ch/api/stac/v1/'
+    out <- content(GET(paste0(bu, 'collections/', id, '/items')))
+    if (is.null(out$code)) {
+        ids <- sapply(out$features, '[[', 'id')
+        nms <- lapply(out$features, \(x) names(x$assets))
+        names(nms) <- ids
+        nms
+    } else {
+        out
+    }
+}
+
+meteoswiss_collections()
+
+xx <- get_items('ch.meteoschweiz.ogd-smn')
+get_items(cl[1])
+
