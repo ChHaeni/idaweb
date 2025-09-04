a5b64dff0ac559a2b85eaff876c0ef54e18a9732
Christoph Häni <christoph.haeni@bfh.ch>
2025-09-04 12:05:59 +0200

    Update and add classes to metadata

--- scripts/idaweb-functions.r
+++ scripts/idaweb-functions.r
@@ -881,47 +881,29 @@ fuzzy_search <- function(x, y, ignore.case = TRUE, value = FALSE) {
     grep(fuzzy_x, y, value = value, ignore.case = ignore.case)
 }
 
+## re-build meta data ----------------------------------------
 
-## testing ----------------------------------------
+if (FALSE) {
+    # check supported collections from MeteoSwiss
+    sup <- supported_collections()
+    sup
 
-# # base url to REST API / GET search
-# valid_collections <- paste(
-#     # 'ch.meteoschweiz.ogd-nbcn',
-#     # 'ch.meteoschweiz.ogd-nbcn-precip',
-#     # 'ch.meteoschweiz.ogd-nime',
-#     # 'ch.meteoschweiz.ogd-obs',
-#     # 'ch.meteoschweiz.ogd-phenology',
-#     # 'ch.meteoschweiz.ogd-pollen',
-#     'ch.meteoschweiz.ogd-smn',
-#     'ch.meteoschweiz.ogd-smn-precip',
-#     'ch.meteoschweiz.ogd-smn-tower',
-#     'ch.meteoschweiz.ogd-tot',
-#     sep = ','
-# )
-# bu <- paste0('https://data.geo.admin.ch/api/stac/v1/search?collections=', 
-#     valid_collections, '&')
-# # t1 <- content(GET(paste0(bu, 'bbox=6.96,45.82,9,46.81')))
-# bern <- c(46.989090, 7.463082)
-# ll <- c(46.979143, 7.445185)
-# ur <- c(46.997198, 7.482103)
-# bbox <- paste(c(rev(ll), rev(ur)), collapse = ',')
-# t1 <- content(GET(paste0(bu, 'bbox=', bbox, '&datetime=2024-01-01T00:00:00Z/..')))
-# # names(t1)
-# length(t1$features)
-# # NOTE: always limit of 100!!
-# names(t1$features[[1]]$assets)
-# str(t1)
+    # get meta data
+    # args(get_metadata)
+    meta_datainv <- get_metadata(sup, 'data', cache_dir = path_cache)
+    meta_stations <- get_metadata(sup, 'stat', cache_dir = path_cache)
+    meta_parameters <- get_metadata(sup, 'par', cache_dir = path_cache)
 
-# https://opendatadocs.meteoswiss.ch/general/download#update-frequency
-# historical    (meas. start until 31.12 last year): once a year        (m, d, h, t)
-# recent        (1.1. current year until yesterday): daily at 12UTC     (m, d, h, t)
-# now           (yesterday 12UTC to now):            every 10 min       (h, t)
-# no type
+    # rebuild meta data
+    metadata <- mapply(\(col, inv, stat, para) {
+        structure(list(
+            assets = structure(col$assets, class = 'ms_assets'),
+            datainventory = structure(inv, class = 'ms_datainventory'),
+            stations = structure(stat, class = 'ms_stations'),
+            parameters = structure(para, class = 'ms_parameters')
+        ), class = 'ms_metadata')}, attr(sup, 'collections'), meta_datainv, 
+        meta_stations, meta_parameters, SIMPLIFY = FALSE)
+    names(metadata) <- sup
+    save(metadata, file = 'data/metadata.rda')
+}
 
-# m: monthly, d: daily, h: hourly, t: 10-min
-
-# for granularity t and h the time stamp defines the end of the measurement interval and
-# for higher granularities (d, m and y) the time stamp defines the beginning of the interval!
-
-# Missing values (e.g. due to instrument failure) are empty fields. 
-# Empty columns are used when a parameter is not measured at all at a certain station.
