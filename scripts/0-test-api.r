a6ed8a3f79fa298fb6c81efcd9c6e3cce30795c9
Christoph Häni <christoph.haeni@bfh.ch>
2025-09-05 10:40:19 +0200

    Check datetime tests

--- scripts/0-test-api.r
+++ scripts/0-test-api.r
@@ -42,21 +42,13 @@ metadata[[1]]
 zz0 <- search_by_datetime('01.01.2018 to 05.02.2018', meta_search = metadata[7])
 zz1 <- search_by_datetime('01.01.2018 to 05.02.2018', meta_search = metadata[10])
 zz1b <- search_by_datetime('01.01.2017 to 01.02.2018', meta_search = zz1)
-xx <- search_by_datetime('01.01.2018 to 05.02.2018', meta_search = metadata)
-
-yy <- search_by_datetime('smn', '13.08.2020')
-
+zz2 <- search_by_datetime('01.01.2018 to 05.02.2018', meta_search = metadata)
 search_by_datetime('13.08.2020', '01.01.2018') # -> error
 zz3 <- search_by_datetime('all', '13.08.2020', tz = 'CET', to = '14.08.2020')
-search_by_datetime('07.02.2024/08.03.2025')
-search_by_datetime(to = '13.08.2020')
-search_by_datetime(from = '01.01.2018', to = '13.08.2020')
-
-
-## hier weiter!!!
-# TODO:
-#   search functions
-#   search by: time range, location range, parameters (fuzzy search)
+search_by_datetime('07.02.2024/08.03.2025') # -> error
+zz4 <- search_by_datetime(from = '07.02.2024/08.03.2025')
+# search_by_datetime(to = '13.08.2020')
+# search_by_datetime(from = '01.01.2018', to = '13.08.2020')
 
 
 # zz1 <- search_by_datetime(metadata[[7]], '01.01.2018 to 05.02.2018')
