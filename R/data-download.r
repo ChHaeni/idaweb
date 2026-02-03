a41e2f836b059de62d08b2911b20d66528d11437
Christoph Häni <christoph.haeni@bfh.ch>
2026-02-03 14:04:21 +0100

    Start roxygen2 comments

--- R/data-download.r
+++ R/data-download.r
@@ -1,8 +1,24 @@
 
 ## download data from search result ----------------------------------------
 
+#' Download data from Meteoswiss (Open Data)
+#'
+#' @param meta_data An object of class \code{met_metadata}. A collection of meta data on the data which should be downloaded.
+#' @param cache_dir character. Path to the directory where the cached data is saved.
+#' @param force_cache logical. Should the argument \code{cache_dir} be taken, even if the cached data has been saved elsewhere during the current R session? Default \code{FALSE}.
+#' @param single_timestamp logical. Should a single time stamp be given in the data (default), or should the start and end (named \code{\sQuote{st}} and \code{\sQuote{et}}) of each interval be provided?
+#' @param outclass character. Class of the returned data. Default is \code{data.frame}. See \code{Details} on how data is provided.
+#' @param outstruc character. How should the returned object be structured? Default is \code{\sQuote{split-all}}. See \code{Details} for available options.
+#' @return Meteoswiss data provided as a \code{list}, except if argument \code{outstruc} is set to \code{\sQuote{cbind-all}} (see \code{Details}).
+#' @details argument \code{outstruc} can be used to structure the output in four different ways.
+#'      \code{\sQuote{split-all}} (default) \code{list} with each entry representing unique combinations of station/granularity.
+#'      \code{\sQuote{by-station}} gathers all granularities for each unique stations as single list entries.
+#'      \code{\sQuote{by-granularity}} gathers all stations for each unique granularity.
+#'      \code{\sQuote{cbind-all}} gathers all data into a single object.
+#'      The lowest level data is of the class provided with \code{outclass} (default is \code{data.frame}).
+#' @export
 get_data <- function(meta_data, cache_dir = tempdir(), force_cache = FALSE, 
-    outclass = c('data.frame', 'data.table', 'ibts', 'df', 'dt'), single_timestamp = TRUE,
+    single_timestamp = TRUE, outclass = c('data.frame', 'data.table', 'ibts', 'df', 'dt'),
     outstruc = c('split-all', 'by-station', 'by-granularity', 'cbind-all')
     ) {
     # check arguments
